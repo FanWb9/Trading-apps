5dd63716bb4b4de01d7153195415b0e8b5460f8c
IRFAN HAKIM <irfanhakim@IRFANs-MacBook-Air.local>
2025-04-12 18:39:35 +0700

    Pages Keuangan

--- src/Pages/LaporanKeuangan.jsx
+++ src/Pages/LaporanKeuangan.jsx
@@ -1,6 +1,6 @@
 export default function LaporanKeuangan() {
     return (
-      <div className="min-h-screen bg-red-500 text-gray-800 p-6">
+      <div className="min-h-screen bg-gray-100 text-gray-800 p-20">
         <h1 className="text-3xl font-bold mb-4">Laporan Keuangan</h1>
         <p className="text-lg">Ini adalah halaman untuk laporan arus kas & neraca.</p>
       </div>
