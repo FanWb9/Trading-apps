8c81190f304713d5aaaf2849040624fd831490ca
FanWb9 <irfanhakimgkh@gmail.com>
2025-04-21 22:59:57 +0700

    location btn

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -118,7 +118,7 @@ export default function Home() {
                       {t("home.buttons.whatsapp")}
                     </button>
 
-                  <button onClick={() => window.localStorage.href = "https://thecoreaccounting.com/"} className="bg-blue-600 px-4 py-2 md:px-5 md:py-2 rounded-md font-semibold hover:bg-blue-700">
+                  <button onClick={() => window.location.href = "https://thecoreaccounting.com/"} className="bg-blue-600 px-4 py-2 md:px-5 md:py-2 rounded-md font-semibold hover:bg-blue-700">
                     {t("home.buttons.freeTrial")}
                   </button>
                 </div>
