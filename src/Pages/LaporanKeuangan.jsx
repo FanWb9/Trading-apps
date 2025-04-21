82ae15debe395e1dee094016bd5bcb2931130d45
FanWb9 <irfanhakimgkh@gmail.com>
2025-04-21 16:04:04 +0700

    laporan

--- src/Pages/LaporanKeuangan.jsx
+++ src/Pages/LaporanKeuangan.jsx
@@ -1,7 +1,7 @@
 export default function LaporanKeuangan() {
     return (
       <div className="min-h-screen bg-gray-100 text-gray-800 p-20">
-        <h1 className="text-3xl font-bold mb-4">Laporan Keuangan company</h1>
+        <h1 className="text-3xl font-bold mb-4">Laporan </h1>
         <p className="text-lg">Ini adalah halaman untuk laporan arus kas & tabungan contoh relod</p>
       </div>
     );
