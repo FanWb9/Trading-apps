6c5017310383aee342a265609ef1cbd26517f4df
FanWb9 <irfanhakimgkh@gmail.com>
2025-04-25 10:34:50 +0700

    test eror

--- src/Pages/Payments/Harga.jsx
+++ src/Pages/Payments/Harga.jsx
@@ -4,7 +4,7 @@ const Harga = () => {
   return (
     <div className="bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 mt-[80px]">
       <div className="max-w-6xl mx-auto">
-        <h2 className="text-center text-3xl font-bold text-gray-900 mb-10">Pilih Paket Sesuai Kebutuhanmuu</h2>
+        <h2 className="text-center text-3xl font-bold text-gray-900 mb-10">Pilih Paket Sesuai Kebutuhanmuuu</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           
           {/* STARTER */}
