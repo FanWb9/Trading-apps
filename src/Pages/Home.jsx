32619ff95cb1de897f1be8593c4a8dbb567f38f6
FanWb9 <irfanhakimgkh@gmail.com>
2025-05-02 21:01:18 +0700

    Add fitur faq

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,8 +8,11 @@ import pic1 from "../assets/pic1.png";
 import pic3 from "../assets/Akuntansi.png";
 import Pic4 from "../assets/4.png";
 import Pic5 from "../assets/2.png";
+import FAQ from "./FAQ";
 import Pic6 from "../assets/1.png";
-import { ShieldCheck, Cloud, Users, LayoutDashboard, LifeBuoy, Layers3,Globe2,  Database } from "lucide-react";
+import { ShieldCheck, Cloud, Users, LayoutDashboard, LifeBuoy, Layers3,Globe2,  Database , MessageCircle, Phone, CalendarDays } from "lucide-react";
+import { ChatBubbleLeftRightIcon, PhoneIcon, CalendarDaysIcon } from '@heroicons/react/24/outline';
+
 export default function Home() {
   const { t } = useTranslation();
   // const [current, setCurrent] = useState(0);
@@ -137,7 +140,7 @@ export default function Home() {
   // }, []);
 
   return (
-    <div >
+    <div className="" >
       <div className="min-h-screen md:min-h-[60vh] lg:min-h-[100vh] bg-white text-gray-900 px-6 md:px-[80px] py-[50px] md:py-[40px] lg:py-[80px] flex flex-col-reverse md:flex-row items-center justify-between gap-8 ">
           
           {/* Kiri: Konten */}
@@ -169,9 +172,8 @@ export default function Home() {
             />
           </div>
         </div>
-        <h2 className=" text-2xl lg:text-3xl font-semibold text-center mb-[15px]">
-            {t("home.items.title")}
-        </h2>
+        <section className="bg-white px-6 md:px-14  sm:h-[450px] md:h-[250px] lg:h-[250px] py-7">
+        <h2 className="text-2xl text-center font-bold">{t("home.items.title")}</h2>
         <div className="overflow-hidden">
           <div className="flex animate-scroll will-change-transform gap-6 min-w-max">
           {[...logos, ...logos, ...logos].map((img,idx) => (
@@ -185,6 +187,7 @@ export default function Home() {
           ))}
           </div>
         </div>
+      </section>
       {/* HERO SLIDESHOW */}
       {/* <div className="relative h-[75vh] sm:h-[40vh] md:h-[70vh] lg:h-screen overflow-hidden bg-black/90 text-white">
         {slides.map((slide, index) => (
@@ -319,6 +322,68 @@ export default function Home() {
           </div>
         </div>
       </section>
+      
+      <section className="min-h-screen md:min-h-[70vh] bg-white py-10">
+        <FAQ/>
+      </section>
+
+      <section className="py-20 bg-white text-center px-4 sm:px-8 md:px-16 min-h-screen md:h-min-[50vh] lg:min-h-[50vh]">
+      <h2 className="text-gray-700 font-semibold mb-2">Hubungi Kami</h2>
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-900 mb-10">
+        Bicarakan Kebutuhan Bisnis Anda Bersama Kami
+      </h1>
+
+      <div className="flex flex-col md:flex-row justify-center items-center md:items-stretch gap-6">
+        {/* Live Chat */}
+        <div className="bg-gray-100 p-6 sm:p-8 rounded-lg shadow-md w-full max-w-md">
+          <div className="flex justify-center mb-4">
+            <MessageCircle className="w-10 h-10 sm:w-12 sm:h-12 text-blue-500" />
+          </div>
+          <h3 className="text-lg sm:text-xl font-semibold mb-2">Live Chat</h3>
+          <p className="text-gray-600 mb-4 sm:mb-6 text-sm sm:text-base">
+            Terhubung langsung dengan konsultan kami melalui web chat atau telepon untuk solusi cepat.
+          </p>
+          <button
+            onClick={() => window.location.href = "https://wa.me/6285215319526?text=Halo%2C%20Saya%20ingin%20tanya%20tentang%20thecore"}
+            className="bg-blue-600 text-white px-4 py-2  rounded-full font-semibold md:mt-[20px] hover:bg-blue-700 w-full"
+          >
+            Chat WhatsApp
+          </button>
+        </div>
+
+        {/* Call Center */}
+        <div className="bg-gray-100 p-6 sm:p-8 rounded-lg shadow-md w-full max-w-md">
+          <div className="flex justify-center mb-4">
+            <Phone className="w-10 h-10 sm:w-12 sm:h-12 text-blue-500" />
+          </div>
+          <h3 className="text-lg sm:text-xl font-semibold mb-2">Call Center</h3>
+          <p className="text-gray-600 mb-4 sm:mb-6 text-sm sm:text-base">
+            Diskusikan berbagai kebutuhan bisnis Anda bersama tim kami. <br />
+            Jam Operasional: 09.00 – 17.00 WIB
+          </p>
+          <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-full w-full font-semibold">
+            (021) 7243085
+          </button>
+        </div>
+
+        {/* Jadwal Meeting */}
+        <div className="bg-gray-100 p-6 sm:p-8 rounded-lg shadow-md w-full max-w-md">
+          <div className="flex justify-center mb-4">
+            <CalendarDays className="w-10 h-10 sm:w-12 sm:h-12 text-blue-500" />
+          </div>
+          <h3 className="text-lg sm:text-xl font-semibold mb-2">Jadwalkan Meeting</h3>
+          <p className="text-gray-600 mb-4 sm:mb-6 text-sm sm:text-base">
+            Pilih waktu terbaik Anda untuk sesi demo pribadi bersama tim profesional kami.
+          </p>
+          <button
+            onClick={() => window.location.href = "https://wa.me/6285215319526?text=Halo%2C%20Saya%20ingin%20tanya%20tentang%20thecore"}
+            className="bg-blue-600 text-white px-4 py-2 rounded-full font-semibold hover:bg-blue-700 w-full md:mt-[25px]"
+          >
+            Chat WhatsApp
+          </button>
+        </div>
+      </div>
+    </section>
     </div>
   );
 }
